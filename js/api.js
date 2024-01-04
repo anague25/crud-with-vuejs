9b725dcf5e5b45cc0bda02c30b3d4342753a6a15
Anague Sonna Roosvelt <anaguesonnaroosvelt@gmail.com>
2024-01-05 00:34:46 +0100

    function to search student and get student

--- js/api.js
+++ js/api.js
@@ -1,5 +1,15 @@
 
+const STUDENTDB = 'StudentDb';
 
+//to get a locale database
+
+
+function getLocalDb(){
+    if(!localStorage.getItem(STUDENTDB)){
+        localStorage.setItem(STUDENTDB,JSON.stringify([]));
+} 
+    return JSON.parse(localStorage.getItem(STUDENTDB));
+}
 
 
 
@@ -20,7 +30,7 @@ function addStudent(student){
 //function to update student
 
 function updateStudent(student){
-    const db = getLocalDB();
+    const db = getLocalDb();
    updatedDb = db.map(function(curStudent){
         if(curStudent.id == student.id){
             return {
@@ -34,4 +44,43 @@ function updateStudent(student){
         return curStudent;
     })
     updateDb(updatedDb);
+}
+
+
+
+//function to delete an element
+
+
+function deleteStudent(student){
+    const db = getLocalDb();
+    const updatedDb = db.filter(function(curStudent){
+        return curStudent.id != student.id;
+    });
+
+
+
+}
+
+
+
+//function to get student trought his id
+
+function getStudent(id){
+    const db = getLocalDb();
+    var filteredDb = db.filter((data)=> data.id != student.id);
+    if(filteredDb.length > 0){
+        return filteredDb[0];
+    }
+    return null;
+}
+
+
+//function to search student
+
+function searchStudentByName(name){
+    const db = getLocalDb;
+    const filteredDb = db.filter((data)=>{
+        return data.nom.toLowercase().include(name.toLowercase()) || data.prenom.toLowercase().include(name.toLowercase()) ;
+    });
+    return filteredDb;
 }
